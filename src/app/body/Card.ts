0f994f09dc9b1581a39b30fe9f2ba1b4a3b95c5d
Harmandeep Singh <deepbhangu729@gmail.com>
2024-04-14 20:38:49 +0530

    updated body component

---
+++ src/app/body/Card.ts
@@ -0,0 +1,6 @@
+export class Card {
+    image: string | undefined
+    heading: string | undefined
+    price: number | undefined
+    link: string | undefined
+}
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,12 +1,96 @@
 import { Component } from '@angular/core';
+import { Card } from './Card';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+  styleUrls: ['./body.component.css']
 })
 export class BodyComponent {
-
+  cards: Card[];
+  constructor() {
+    this.cards = [
+      {
+        image: "assets/images/laptop-bag-1.webp",
+        heading: 'Laptop Bag 1',
+        price: 50.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/laptop-bag-2.webp",
+        heading: 'Laptop Bag 2',
+        price: 35.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/laptop-bag-3.webp",
+        heading: 'Laptop Bag 3',
+        price: 50.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/laptop-bag-4.jpg",
+        heading: 'Laptop Bag 4',
+        price: 35.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/Lovevook-laptop-bag.webp",
+        heading: 'Lovevook Laptop Bag',
+        price: 36.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/Voova-laptop-bag.webp",
+        heading: 'Voova Laptop Bag',
+        price: 19.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/women-laptop-bag.webp",
+        heading: 'Stylish Laptop Bag',
+        price: 33.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/Ytonet-laptop-bag.webp",
+        heading: 'Ytonet Laptop Bag',
+        price: 15.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/Bagsmart-laptop-bag.webp",
+        heading: 'Ytonet Laptop Bag',
+        price: 15.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/Dachee-laptop-bag.webp",
+        heading: 'Ytonet Laptop Bag',
+        price: 15.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/Lovevook-laptop-bag-2.webp",
+        heading: 'Ytonet Laptop Bag',
+        price: 15.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/Lovevook-laptop-bagpack.webp",
+        heading: 'Ytonet Laptop Bag',
+        price: 15.00,
+        link: "#"
+      },
+      {
+        image: "assets/images/Mosiso-laptop-bag.webp",
+        heading: 'Ytonet Laptop Bag',
+        price: 15.00,
+        link: "#"
+      },
+    ]
+  }
 }
