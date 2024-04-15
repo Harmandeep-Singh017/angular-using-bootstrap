8cabc9a7e00ae36fa1e7a7299daf785ad94d2737
Harmandeep Singh <deepbhangu729@gmail.com>
2024-04-15 19:28:13 +0530

    Updated all components HTML code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { SubHeaderComponent } from './sub-header/sub-header.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
@@ -10,7 +12,7 @@ import { SubHeaderComponent } from './sub-header/sub-header.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, BodyComponent, SubHeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, SubHeaderComponent, BodyComponent, ContactUsComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'Ekart';
